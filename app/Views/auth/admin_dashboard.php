4725d54c8f5d6b365dcebd7ea6823d0bc470ab8c
warminski <35105074+warminski@users.noreply.github.com>
2020-06-18 18:05:17 +0200

    fixed calendar css

--- app/Views/auth/admin_dashboard.php
+++ app/Views/auth/admin_dashboard.php
@@ -51,9 +51,29 @@ $asdasd = array_values($dbdatanames);
     .dataCell{
         border: 1px solid black;
     }
-    td, th{
-        border: 1px solid black;
-        padding: 3px;
+
+    table, td, th {
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 10px;
+    }
+
+    th>.truncate, td>.truncate{
+        width: auto;
+        min-width: 0;
+        max-width: 200px;
+        display: inline-block;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 
 </style>
